dad97de997172cf8c9ab66f45c9f5fbc153fd41f
Kolin Keane <kjkeane@protonmail.com>
2019-04-13 23:48:52 -0400

    Add vim plugin script

---
+++ vim/plugins.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+#Create new folder in ~/.vim/pack that contains a start folder and cd into it
+#
+# Arugments:
+#   package_group, a string folder name to create and change into.
+#
+# Examples:
+#   set_group synta-highlighting
+#
+function set_group () {
+  package_group=$1
+  path="$HOME/.vim/pack/$package_group/start"
+  mkdir -p "$path"
+  cd "$path" || exit
+}
+
+# Clone or update a git repo in the current directory
+#
+# Arguments:
+#   repo_url, a URL to the git repo.
+#
+# Examples:
+#   package https://github.com/saltstack/salt-vim.git
+#
+function package () {
+  repo_url=$1
+  expected_repo=$(basename "$repo_url" .git)
+  if [ -d "$expected_repo" ];
+  then
+    cd "$expected_repo" || exit
+    result=$(git pull --force)
+    echo "$expected_repo: $result"
+  else
+    echo "$expected_repo: Installing..."
+    git clone -q "$repo_url"
+  fi
+}
+
+(
+set_group plugins
+package https://github.com/saltstack/salt-vim.git &
+wait
+) &
+
+wait
